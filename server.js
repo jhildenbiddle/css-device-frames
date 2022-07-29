0a0dc62333d91cb001abb6113cc87b1fc379a9f8
John Hildenbiddle <john@hildenbiddle.com>
2022-07-29 16:32:02 -0500

    Fix CDN replacement URL regex

--- server.js
+++ server.js
@@ -35,12 +35,16 @@ browserSync.init({
     rewriteRules: [
         // Replace CDN URLs with local paths
         {
-            match  : /https:\/\/cdn\.jsdelivr\.net\/npm\/css-device-frames\/dist\//g,
-            replace: ''
+            match  : /https?.*\/CHANGELOG.md/g,
+            replace: '/CHANGELOG.md'
         },
         {
-            match  : /https:\/\/raw\.githubusercontent\.com\/jhildenbiddle\/css-device-frames\/master\//g,
-            replace: ''
+            // CDN paths to local paths
+            // Ex1: //cdn.com/package-name/path/file.js => /path/file.js
+            // Ex2: http://cdn.com/package-name@1.0.0/dist/file.js => /dist/file.js
+            // Ex3: https://cdn.com/package-name@latest/dist/file.js => /dist/file.js
+            match  : /(?:https?:)*\/\/.*cdn.*css-device-frames[@\d.latest]*\/(?:dist\/)/g,
+            replace: '/'
         }
     ]
 });
